f189677e507a72c102fc8b3451d76c722d04f60f
Sunit <sunit@SunitdeMBP.lan>
2021-03-24 17:32:38 +0800

    Point backend to correct TFC org and workspace

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
       version = "3.0.1"
     }
   }
-  required_version = "~> 0.14"
+  required_version = "~> 0.13"
 
   backend "remote" {
     organization = "mason105"
@@ -25,17 +25,17 @@ data "alicloud_images" "default" {
   most_recent = true
   owners      = "system"
   name_regex  = "^ubuntu.*18.*64"
-  os_type = "linux"
+  os_type     = "linux"
   output_file = "images.json"
 }
 
 data "alicloud_regions" "current_region_ds" {
- output_file = "regions.json"
+  output_file = "regions.json"
 }
 
 data "alicloud_zones" "zones_ds" {
-  available_resource_creation = "VSwitch" 
-  output_file = "zones.json"
+  available_resource_creation = "VSwitch"
+  output_file                 = "zones.json"
 }
 
 resource "alicloud_vpc" "hkgw" {
@@ -46,17 +46,17 @@ resource "alicloud_vpc" "hkgw" {
 resource "alicloud_vswitch" "hkgwsw" {
   vpc_id            = alicloud_vpc.hkgw.id
   cidr_block        = "192.168.1.0/24"
-  availability_zone = "${data.alicloud_zones.zones_ds.zones.0.id}"
+  availability_zone = data.alicloud_zones.zones_ds.zones.0.id
 }
 
 resource "alicloud_security_group" "hkgw_sg" {
-  name = "hkgw_sg"
+  name   = "hkgw_sg"
   vpc_id = alicloud_vpc.hkgw.id
 }
 
 resource "alicloud_key_pair" "publickey" {
   key_name   = "my_public_key"
-  public_key = file("~/.ssh/id_rsa.pub") 
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "alicloud_security_group_rule" "allow_all_tcp" {
@@ -78,10 +78,10 @@ data "template_file" "user_data" {
 data "template_cloudinit_config" "config" {
   gzip          = false
   base64_encode = false
-  part{
-   filename     = "init.cfg"
+  part {
+    filename     = "init.cfg"
     content_type = "text/cloud-config"
-    content      = "${data.template_file.user_data.rendered}"
+    content      = data.template_file.user_data.rendered
   }
   part {
     content_type = "text/x-shellscript"
@@ -90,29 +90,29 @@ data "template_cloudinit_config" "config" {
 }
 
 resource "alicloud_instance" "instance" {
-   count 		= var.number
-   instance_name	= "${var.short_name}-${var.role}-${format(var.count_format, count.index + 1)}"
-   host_name	 	= "${var.short_name}-${var.role}-${format(var.count_format, count.index + 1)}"
-   image_id		= data.alicloud_images.default.images.0.id
-   instance_type	= var.instance_type
-   security_groups      = alicloud_security_group.hkgw_sg.*.id
-   vswitch_id           = alicloud_vswitch.hkgwsw.id
+  count           = var.number
+  instance_name   = "${var.short_name}-${var.role}-${format(var.count_format, count.index + 1)}"
+  host_name       = "${var.short_name}-${var.role}-${format(var.count_format, count.index + 1)}"
+  image_id        = data.alicloud_images.default.images.0.id
+  instance_type   = var.instance_type
+  security_groups = alicloud_security_group.hkgw_sg.*.id
+  vswitch_id      = alicloud_vswitch.hkgwsw.id
 
-   internet_charge_type       = var.internet_charge_type
-   internet_max_bandwidth_out = var.internet_max_bandwidth_out
+  internet_charge_type       = var.internet_charge_type
+  internet_max_bandwidth_out = var.internet_max_bandwidth_out
 
-   spot_strategy 	= "SpotAsPriceGo"
-   password = var.ecs_password
-   key_name = alicloud_key_pair.publickey.key_name
-   instance_charge_type          = "PostPaid"
-   system_disk_category          = "cloud_efficiency"
-   system_disk_size              = 400
-   security_enhancement_strategy = "Deactive"
-   tags = {
-      create = "terraform",
-      role   = "k8s-worker"
-    }
-   user_data                   = data.template_file.user_data.rendered
+  spot_strategy                 = "SpotAsPriceGo"
+  password                      = var.ecs_password
+  key_name                      = alicloud_key_pair.publickey.key_name
+  instance_charge_type          = "PostPaid"
+  system_disk_category          = "cloud_efficiency"
+  system_disk_size              = 400
+  security_enhancement_strategy = "Deactive"
+  tags = {
+    create = "terraform",
+    role   = "k8s-worker"
+  }
+  user_data = data.template_file.user_data.rendered
 }
 
 
